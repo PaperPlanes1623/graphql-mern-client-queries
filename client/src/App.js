eb558650b3d118ec0493ff292f69551d6dc132c3
Dylan <dtaft@onlineimage.com>
2023-01-15 15:41:56 -0800

    react state form set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from "./components/Clients";
+import AddClientModal from "./components/addClientModal";
+
+//remove warning for cache data refetch - Not working?
+const cache = new InMemoryCache({
+  typePolicies: {
+    query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql', 
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -13,6 +34,7 @@ function App() {
     <ApolloProvider client={client}>
     <Header />
     <div className="container">
+      <AddClientModal />
       <Clients /> 
     </div>
     </ApolloProvider>
