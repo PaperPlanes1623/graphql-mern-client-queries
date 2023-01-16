ea44d29dae15703ad80b95bc697333b4e9d596e6
Dylan <dtaft@onlineimage.com>
2023-01-15 17:04:48 -0800

    adding project page props

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
+import Project from './pages/Project';
 import NotFound from './pages/NotFound';
 
 //remove warning for cache data refetch - Not working?
@@ -38,6 +39,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path="/projects/:id" element={<Project />}></Route>
           <Route path="*" element={<NotFound /> }></Route>
         </Routes>
       </div>
